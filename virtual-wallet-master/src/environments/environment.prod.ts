4eca2ad856e44e1ae2119c6eb723229307767084
Antonio Psanchez <antoniosanest23@gmail.com>
2023-01-26 10:36:42 +0100

    subiendo virtual

---
+++ virtual-wallet-master/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDMkP5TxP6HQ7ONnXn6fNCYka1dJT8BaRE",
+    authDomain: "my-wallet-b1889.firebaseapp.com",
+    projectId: "my-wallet-b1889",
+    storageBucket: "my-wallet-b1889.appspot.com",
+    messagingSenderId: "5366040021",
+    appId: "1:5366040021:web:f6ae5cbbf00f95b3f1a95c",
+    
+  }
+  
+};
---
+++ virtual-wallet-master/src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDMkP5TxP6HQ7ONnXn6fNCYka1dJT8BaRE",
+    authDomain: "my-wallet-b1889.firebaseapp.com",
+    projectId: "my-wallet-b1889",
+    storageBucket: "my-wallet-b1889.appspot.com",
+    messagingSenderId: "5366040021",
+    appId: "1:5366040021:web:f6ae5cbbf00f95b3f1a95c",
+  }
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
